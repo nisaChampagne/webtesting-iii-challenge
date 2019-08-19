334ccaf6c81bb30eedf9c9afcc2d7bd140422219
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-19 15:55:57 -0400

    dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -5,3 +5,12 @@ import { render } from '@testing-library/react'
 import '@testing-library/react/cleanup-after-each'
 import Dashboard from './Dashboard'
 
+describe('Dashboard test', () => {
+    it('should render controls & display',() => {
+        const { getByText } = render(<Dashboard />)
+        getByText(/unlocked/i)
+        getByText(/lock gate/i)
+        getByText(/open/i)
+        getByText(/close gate/i)
+    })
+})
